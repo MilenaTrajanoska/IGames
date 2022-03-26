443c32d24724d4a8c30a6ab1e4e3c98652d4dd95
ivonaandova <74076663+ivonaandova@users.noreply.github.com>
2022-03-26 22:56:24 +0100

    Created User.cs model

---
+++ IGames.Domain/Identity/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace IGames.Domain.Identity
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual Cart UserCart { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
