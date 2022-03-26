6ad2a1a11ff634a4290b408b7d677ccbeb0b06c2
ebru <ebru.2000@live.com>
2022-03-26 20:59:57 +0100

    created role and VideoGame model

---
+++ IGames.Domain/DomainModels/VideoGame.cs
@@ -0,0 +1,20 @@
+﻿using FiftyOne.Foundation.Mobile.Detection.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketShop.Domain.DomainModels
+{
+    public class VideoGame : BaseEntity
+    {
+        public string GameTitle { get; set; }
+        public string Image { get; set; }
+        public GenreEnum Genre { get; set; }
+
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<VideoGameInShoppingCart> TicketsInShoppingCart { get; set; }
+        public IEnumerable<VideoGameInOrder> VideoGamesInOrder { get; set; }
+    }
+}
