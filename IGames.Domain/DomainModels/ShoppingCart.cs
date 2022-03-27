071c4047ec1c848f59c35c5609d6f8dcb3c6b59a
davidgagaleski <davidgagaleski@gmail.com>
2022-03-27 13:20:34 +0200

    base entity and shopping cart

---
+++ IGames.Domain/DomainModels/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGames.Domain.DomainModels
+{
+    public class ShoppingCart : BaseEntity
+    {
+        public string UserId { get; set; }
+        public User UserOwner { get; set; }
+        public virtual ICollection<VideoGameInShoppingCart> VideoGamesInShoppingCart { get; set; }
+    }
+}
