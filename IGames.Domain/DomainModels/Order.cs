0db5860128525c16ffe70cfbc6ff18903fb495e9
sklincharov <45566246+SasheKlincharov@users.noreply.github.com>
2022-03-28 22:27:04 +0200

    created genreEnum and Order

---
+++ IGames.Domain/DomainModels/Order.cs
@@ -0,0 +1,15 @@
+namespace IGames.Domain.DomainModels
+{
+    public enum GenreEnum
+    {
+        HORROR,
+        COMEDY,
+        ROMANCE,
+        DRAMA,
+        THRILLER,
+        SCI_FI,
+        MISTERY,
+        KIDS,
+        ACTION
+    }
+}
