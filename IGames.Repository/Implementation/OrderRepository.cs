66be1d40c635c130fc7ea663e76e7f634defe674
ebru <ebru.2000@live.com>
2022-04-13 21:16:08 +0200

    Order Repo Interface and Impl

---
+++ IGames.Repository/Implementation/OrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using IGames.Domain.DomainModels;
+using IGames.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace IGames.Repository.Implementation
+{
+    public class OrderRepository : IOrderRepository
+    {
+
+        private readonly ApplicationDbContext context;
+        private DbSet<Order> orders;
+
+        public OrderRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+            orders = context.Set<Order>();
+        }
+
+        public List<Order> getAllOrders()
+        {
+            return orders
+                .Include(o => o.ApplicationUser)
+                .Include(o => o.VideoGamesInOrder)
+                .Include("TicketsInOrder.ticket")
+                .ToListAsync().Result;
+        }
+
+        public Order getOrderDetails(BaseEntity model)
+        {
+            return orders
+               .Include(o => o.ApplicationUser)
+               .Include(o => o.VideoGamesInOrder)
+               .Include("VideoGamesInOrder.ticket")
+               .SingleOrDefaultAsync(o => o.Id == model.Id).Result;
+        }
+    }
+}
---
+++ IGames.Repository/Interface/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using IGames.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace IGames.Repository.Interface
+{   
+    public interface IOrderRepository
+    {
+        List<Order> getAllOrders();
+        Order getOrderDetails(BaseEntity model);
+    }
+}
