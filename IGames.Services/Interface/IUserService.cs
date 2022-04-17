6d24789077a8af935adcaf76e05720fe38c6bdff
zoricashumarova <shumarovazorica@gmail.com>
2022-04-17 14:49:16 +0200

    Implementing changes in UserService (Interface and Implementation)

---
+++ IGames.Services/Interface/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using IGames.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGames.Services.Interface
+{
+    interface IUserService
+    {
+        List<string> createUsersFromFile(string filePath);
+        public IEnumerable<User> getAllUsers();
+    }
+}
